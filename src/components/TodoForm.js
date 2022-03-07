1d1bfebf24c50cbed72292998c627abfe019cecb
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-07 23:27:44 +0330

    complete the todoForm component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,19 +1,44 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const TodoForm = () => {
-  const [todo, setTodo] = useState("");
+const TodoForm = (props) => {
+  const [input, setInput] = useState(props.edit ? props.edit.text : "");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   const changeHandler = (e) => {
-    console.log(e.target.value);
-    setTodo(e.target.value);
+    setInput(e.target.value);
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
+    if (!input) {
+      alert("enter todo");
+      return;
+    }
+    props.submitTodo(input);
+    setInput("");
   };
+
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" value={todo} onChange={changeHandler} />
-      <button type="submit">Add</button>
+      <div className="formControl">
+        <input
+          type="text"
+          value={input}
+          onChange={changeHandler}
+          placeholder={props.edit ? "update value ..." : "add new todo ..."}
+          ref={inputRef}
+        />
+        <button
+          className={`btn ${props.edit ? "updateTodo" : "addTodo"}`}
+          type="submit"
+        >
+          {props.edit ? "Update" : "Add"}
+        </button>
+      </div>
     </form>
   );
 };
