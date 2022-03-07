307a5d71e849902519331c55325b7351ecf6edfc
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-07 23:23:37 +0330

    add navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const NavBar = ({ unCompletedTodos, onSelect, status }) => {
+  if (!unCompletedTodos) return <h2>set your today todos !</h2>;
+
+  return (
+    <header>
+      <span> {unCompletedTodos}</span> <h2> are not completed </h2>
+      <select onChange={onSelect} value={status}>
+        <option value="All">All</option>
+        <option value="Completed">Completed</option>
+        <option value="Uncompleted">Uncompleted</option>
+      </select>
+    </header>
+  );
+};
+
+export default NavBar;
