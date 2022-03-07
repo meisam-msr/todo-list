c0ab7e9088d578dae98c88ab655f0ece68452320
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-07 23:51:23 +0330

    use react-select

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import Select from "react-select";
 
-const NavBar = ({ unCompletedTodos, onSelect, status }) => {
+const options = [
+  { value: "All", label: "All" },
+  { value: "Completed", label: "Completed" },
+  { value: "Uncompleted", label: "Uncompleted" },
+];
+
+const NavBar = ({ unCompletedTodos, onChange, selectedOption }) => {
   if (!unCompletedTodos) return <h2>set your today todos !</h2>;
 
   return (
     <header>
       <span> {unCompletedTodos}</span> <h2> are not completed </h2>
-      <select onChange={onSelect} value={status}>
+      <Select onChange={onChange} value={selectedOption} options={options} />
+      {/* <select onChange={onSelect} value={status}>
         <option value="All">All</option>
         <option value="Completed">Completed</option>
         <option value="Uncompleted">Uncompleted</option>
-      </select>
+      </select> */}
     </header>
   );
 };
