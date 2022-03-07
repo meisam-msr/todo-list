7c5586ead4b17d0c5261ae7bb419e6be841e3027
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-07 23:24:20 +0330

    add style to todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,16 @@
 const Todo = ({ todo, onComplete, onEdit, onDelete }) => {
   return (
     <div className="todo">
-      <div onClick={onComplete} className={todo.isCompleted ? "completed" : ""}>
+      <div onClick={onComplete} className={`todoText ${todo.isCompleted ? "completed" : ""}`}>
         {todo.text}
       </div>
       <div>
-        <button onClick={onEdit}>Edit</button>
-        <button onClick={onDelete}>Delete</button>
+        <button className="btn" onClick={onEdit}>
+          Edit
+        </button>
+        <button className="btn remove" onClick={onDelete}>
+          Delete
+        </button>
       </div>
     </div>
   );
